29386619b4740937dcee30c7fccab32a3604c5b9
samnangma <masamnang6@gmail.com>
2023-12-06 14:07:20 +0700

    save the code

--- lib/web_view.dart
+++ lib/web_view.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewExample extends StatefulWidget {
-
-  String _currentUrl;
+  final String _currentUrl;
 
   WebViewExample(this._currentUrl);
 
@@ -13,7 +12,6 @@ class WebViewExample extends StatefulWidget {
 
 class _WebViewExampleState extends State<WebViewExample> {
   late WebViewController _webViewController;
-  String _currentUrl = "";
 
   @override
   Widget build(BuildContext context) {
@@ -34,18 +32,31 @@ class _WebViewExampleState extends State<WebViewExample> {
           // This callback is called when the WebView finishes loading a page
           print("Page finished loading: $url");
 
+          // Check if the current URL contains the authorization code
+          try {
+            Uri uri = Uri.parse(url);
+            String code = uri.queryParameters['code'] ?? '';
+
+            // Handling the extracted authorization code
+            if (code.isNotEmpty) {
+              print("Authorization Code: $code");
+
+              // Perform actions with the authorization code (e.g., send it to the server)
+            } else {
+              print("Error: Authorization code not found in the URL");
+            }
+          } catch (e) {
+            print("Error: ${e.toString()}");
+          }
+
           // Check if the current URL is the return URL you expect after login
           if (url.startsWith('http://127.0.0.1:9090/login/oauth2/code')) {
-            setState(() {
-              _currentUrl = url;
-            });
-
             _webViewController.clearCache();
 
             Navigator.pop(context);
           }
         },
-      )
+      ),
     );
   }
 }
